94ae634bd989954e323575f0d2f217086761ada9
lilililililill <111355405+lilililililill@users.noreply.github.com>
2022-08-22 19:49:19 +0900

    app

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('기숙사수용현황분석.csv')
-st.dataframe(data)
+#st.dataframe(data)
 
-st.write('기숙사현황')
+#st.write('기숙사현황')
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
 
