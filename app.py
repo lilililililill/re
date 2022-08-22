bbf256b0eaf5d48c38614df1834fd0119141bf1f
lilililililill <111355405+lilililililill@users.noreply.github.com>
2022-08-22 19:50:16 +0900

    app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ data = pd.read_csv('기숙사수용현황분석.csv')
 #st.write('기숙사현황')
 
 col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+col1.metric("전국대학수", "208")
+col2.metric("국공립학교", "9 mph", "-8%")
+col3.metric("사립", "86%", "4%")
 
