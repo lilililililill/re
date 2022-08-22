2be067d118a87adcc169e9cb000434294f922dba
lilililililill <111355405+lilililililill@users.noreply.github.com>
2022-08-22 19:44:30 +0900

    app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# re
+# streamlit.re
+##streamlit을 이용한 파이썬 웹 서비스
++ github 로그인
++ streamlit 서비스 연결
--- app.py
+++ app.py
@@ -2,4 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+data = pd.read_csv('lilililililill/re/기숙사수용현황분석.csv')
+st.datafram(data)
+
 st.write('기숙사현황')
+
