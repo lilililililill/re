2b2e288b62b53695c752a72f8ffc5d175b9eb912
lilililililill <111355405+lilililililill@users.noreply.github.com>
2022-08-22 20:14:05 +0900

    app

--- app.py
+++ app.py
@@ -7,8 +7,21 @@ data = pd.read_csv('기숙사수용현황분석.csv')
 
 #st.write('기숙사현황')
 
+df. data[data['학교종류']=='대학교']
+num1= len(df['학교'].unique())
+num2= len(df[df['설립구분'] !=  '사립']['학교'].unique())
+num3 = len(df[df['설립구분'] == '사립']['학교'].unique())
+
 col1, col2, col3 = st.columns(3)
-col1.metric("전국대학수", "208")
-col2.metric("국공립학교", "9 mph", "-8%")
-col3.metric("사립", "86%", "4%")
+col1.metric("전국대학수", num1, "")
+col2.metric("국공립학교", num2, num2/num1 * 100)
+col3.metric("사립", num3, num3/num1 * 100)
+
+st.sidebar:
+    gunum = st.radio("학교 구분",("사립", "국공립"))
+    if gunum == '사립'
+        st.write(gubun)
+    else:
+        st.write(gubun)
+
 
